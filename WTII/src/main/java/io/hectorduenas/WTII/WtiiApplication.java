fa0737ea0834532c0e54d791c9905bac83b1edf5
Hector Duenas <hectorduenas.hd@gmail.com>
2021-04-09 20:47:14 -0700

    Added index.html for webpage

--- WTII/src/main/java/io/hectorduenas/WTII/WtiiApplication.java
+++ WTII/src/main/java/io/hectorduenas/WTII/WtiiApplication.java
@@ -1,13 +1,13 @@
-package io.HectorDuenas.Who.s.Turn.Is.It;
+package io.hectorduenas.WTII;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Application {
+public class WtiiApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
+		SpringApplication.run(WtiiApplication.class, args);
 	}
 
 }
--- WTII/src/test/java/io/hectorduenas/WTII/WtiiApplicationTests.java
+++ WTII/src/test/java/io/hectorduenas/WTII/WtiiApplicationTests.java
@@ -1,10 +1,10 @@
-package io.HectorDuenas.Who.s.Turn.Is.It;
+package io.hectorduenas.WTII;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ApplicationTests {
+class WtiiApplicationTests {
 
 	@Test
 	void contextLoads() {
