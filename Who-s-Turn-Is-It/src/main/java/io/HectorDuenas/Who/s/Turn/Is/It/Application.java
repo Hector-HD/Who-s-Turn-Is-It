bc3acb747d4f03dab533c8b2f3a2506372ac6358
Hector Duenas <hectorduenas.hd@gmail.com>
2021-04-09 18:15:48 -0700

    Initial Spring SetUp - Web, ThymeLeaf, H2 DB, OAuth2

---
+++ Who-s-Turn-Is-It/src/main/java/io/HectorDuenas/Who/s/Turn/Is/It/Application.java
@@ -0,0 +1,13 @@
+package io.HectorDuenas.Who.s.Turn.Is.It;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
